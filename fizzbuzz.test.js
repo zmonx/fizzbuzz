bd5d511bb5deef692c7c4fb94ac926b7ab1abd63
ZMONX_GG <chumpitak@gmail.com>
2019-09-22 09:34:17 +0700

    fizzbuzz

---
+++ fizzbuzz.test.js
@@ -0,0 +1,8 @@
+describe("Test FizzBuzz", () => {
+  Test("Test 1 expect 1", () => {
+    const result = FizzBuzz.say(1);
+    expect(result).toEqual(1); // Assert Function
+  });
+});
+
+//npm install -g jest
